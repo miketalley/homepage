288aa73f0b4ff4bbdef7d11a9bd19b64e331e440
Mike Talley <miketalley@hotmail.com>
2019-05-21 22:20:56 -0400

    broke out toolbar into component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,6 @@
 <template>
   <v-app dark>
-    <v-toolbar app>
-      <v-toolbar-side-icon hidden-md-and-up>
-
-      </v-toolbar-side-icon>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Mike Talley</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn flat hidden-sm>
-        Foo
-      </v-btn>
-      <v-btn flat hidden-sm>
-        Foo
-      </v-btn>
-      <v-btn flat hidden-sm>
-        Foo
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="mailto:michaeldtalley@gmail.com"
-        target="_blank"
-      >
-        <span class="mr-2">Email Mike</span>
-      </v-btn>
-    </v-toolbar>
+    <toolbar />
 
     <v-content>
       <particles-container>
@@ -40,12 +15,14 @@
 <script>
 import MainContent from '@/components/MainContent.vue';
 import ParticlesContainer from '@/components/ParticlesContainer.vue';
+import Toolbar from '@/components/Toolbar.vue';
 
 export default {
   name: 'App',
   components: {
     MainContent,
-    ParticlesContainer
+    ParticlesContainer,
+    Toolbar
   },
   data() {
     return {};
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-toolbar app>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn
+        v-for="item in menuItems"
+        :key="item.title"
+        :to="item.url"
+        flat
+      >{{ item.title }}</v-btn>
+    </v-toolbar-items>
+    <v-menu class="hidden-md-and-up">
+      <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+      <v-list>
+        <v-list-tile v-for="item in menuItems" :key="item.title">
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+    <v-spacer></v-spacer>
+    <v-btn
+      flat
+      href="mailto:michaeldtalley@gmail.com"
+      target="_blank"
+    >
+      <span class="mr-2">Email Mike</span>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuItems: [
+        {
+          title: 'Resume',
+          url: ''
+        },
+        {
+          title: 'Github Projects',
+          links: [
+            {
+              title: '',
+              url: ''
+            }
+          ]
+        },
+        {
+          title: 'App Examples',
+          links: [
+            {
+              title: '',
+              url: ''
+            }
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>

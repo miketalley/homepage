a8a9950722414695590a23a2df1a61fe4bfec547
Mike Talley <miketalley@hotmail.com>
2019-05-22 21:06:06 -0400

    added parallax to main content

---
+++ src/components/ParallaxFlex.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-flex :class="marginBottom" ref="container">
+    <div :data-depth="depth">
+      <slot />
+    </div>
+  </v-flex>
+</template>
+
+<script>
+import Parallax from 'parallax-js';
+
+export default {
+  props: {
+    depth: {
+      type: Number,
+      required: true
+    },
+    mb: {
+      type: Number,
+      required: false
+    }
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    const { container } = this.$refs;
+
+    // eslint-disable-next-line no-new
+    new Parallax(container, {
+      relativeInput: true
+    });
+  },
+  computed: {
+    marginBottom() {
+      return `mb-${this.mb}`;
+    }
+  }
+};
+</script>
